dad10c8c49c9053e662cf252841b5e851689c139
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-13 13:53:48 -0400

    added dependencies, express configuration, and sequelize configuration in server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const auth = require('./utils/auth');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+//refactor to support heroku deployment
+const PORT = process.env.PORT || 3001;
+
+//for login authorization
+const hbs = exphbs.create({ auth });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//uncomment when we add public folder
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
