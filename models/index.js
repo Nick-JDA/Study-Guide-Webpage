afb21e362252fe8559311c91dfb3f8b5e1269b17
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-14 11:32:44 -0400

    added relationships between models. Added password and bcrypt hooks to users model.

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const Users = require("./Users");
+const Topics = require("./Users");
+const Modules = require("./Users");
+const Comments = require("./Comments");
+
+// modules have many topics
+Modules.hasMany(Topics, {
+  foreignKey: "module_id",
+});
+// topics have one module
+Topics.belongsTo(Modules, {
+  foreignKey: "module_id",
+});
+// users have many comments
+Users.hasMany(Comments, {
+  foreignKey: "user_id",
+});
+// comments have one user and one topic
+Comments.belongsTo(Users, {
+  foreignKey: "user_id",
+});
+// topics have many comments
+Topics.hasMany(Comments, {
+  foreignKey: "topic_id",
+});
+Comments.belongsTo(Topics, {
+  foreignKey: "topic_id",
+});
