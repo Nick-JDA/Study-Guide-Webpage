ee6bbbf932eb644deba5b1500f4a3838a1dab0ec
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-14 01:24:45 -0400

    added comments model

---
+++ models/Comments.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comments extends Model {}
+
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    topic_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "topic",
+        key: "id",
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comments;
