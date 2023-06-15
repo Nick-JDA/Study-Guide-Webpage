64f59550525b291d86c566f62703537cd4d6b90f
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-15 10:29:45 -0400

    modified homeRoutes to display topics in dropdown bar on main view. Added seeding for (some) topic data.

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,11 +2,16 @@ const sequelize = require('../config/connection');
 const { Users, Modules, Topics } = require('../models');
 
 const moduleData = require('./moduleData.json');
+const topicData = require('./topicData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
   //seed modules
   const users = await Modules.bulkCreate(moduleData, {
+    individualHooks: true,
+    returning: true,
+  });
+  const topics = await Topics.bulkCreate(topicData, {
     individualHooks: true,
     returning: true,
   });
