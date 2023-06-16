a8eb3af1f99f04dc39a0a79b56ae3d6dca450f86
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-16 02:45:39 -0400

    refactored handlebars to incorperate partials. updated api routes to render topics given a topic route.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const commentRoutes = require('./commentRoutes');
 const userRoutes = require('./userRoutes');
+const topicRoutes = require('./topicRoutes');
 
 //uncomment when apiRoutes is set up
 
 router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
-
+router.use('/topics', topicRoutes);
 module.exports = router;
---
+++ controllers/api/topicRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const { Topics, Comments, Modules } = require('../../models');
+
+router.get('/:id', async (req, res) => {
+  try {
+    const topicData = await Topics.findAll({
+      where: { id: req.params.id },
+      include: [{ model: Comments }],
+    });
+    const topic = topicData.map((topic) => topic.get({ plain: true }));
+
+    const moduleData = await Modules.findAll({
+      include: [{ model: Topics, include: [Comments] }],
+    });
+    const modules = moduleData.map((module) => module.get({ plain: true }));
+    console.log(topic);
+    console.log(modules);
+    res.render('homepage', {
+      modules,
+      topic,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
